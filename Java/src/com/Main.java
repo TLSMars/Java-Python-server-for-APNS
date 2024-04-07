3fa4b8dc27d5a517a430366127913cdc4be20dda
TLSMars <TLSMars009@gmail.com>
2024-04-07 09:18:17 +0800

    Update package name and refactor main method in Main.java

--- Java/src/com/Main.java
+++ Java/src/com/Main.java
@@ -1,4 +1,4 @@
-package com;
+package com.qzh;
 
 import java.io.IOException;
 import java.security.KeyStoreException;
@@ -6,15 +6,25 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
-
 import org.json.JSONException;
 
 public class Main {
-    public static void main(String[] args) throws KeyStoreException, NoSuchAlgorithmException, CertificateException,
-            UnrecoverableEntryException, InvalidKeySpecException, IOException, JSONException {
-        String deviceToken = "ced1949617ee5c2f827de51072bf3626b1ed8bdfb09e851631427eb0487f59d5";
-        sendNotificationToAPNS sendNotificationToAPNS = new sendNotificationToAPNS().setDev();
-        sendNotificationToAPNS.sendNotification(deviceToken,
-                "Hello, this is a test message from xxx", "subtitle", "body", 1);
+
+  public static void main(String[] args)
+    throws KeyStoreException, NoSuchAlgorithmException, CertificateException, UnrecoverableEntryException, InvalidKeySpecException, IOException, JSONException {
+    
+    String deviceToken =
+        "ced1949617ee5c2f827de51072bf3626b1ed8bdfb09e851631427eb0487f59d5";
+    String title = "Title";
+    String subTitle = "";
+    String body = "Hello, this is a test message ";
+    Integer badge = 1;
+    sendNotificationToAPNS sendNotificationToAPNS = new sendNotificationToAPNS().setDev();
+    sendNotificationToAPNS.sendNotification(
+        deviceToken,
+        title,
+        subTitle,
+        body,
+    );
     }
 }
